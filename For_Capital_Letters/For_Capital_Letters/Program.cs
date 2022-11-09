c4ea594442c42e6c55493006f75c5393fd47d03e
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-11-09 14:48:52 +0100

    Add files via upload

---
+++ For_Capital_Letters/For_Capital_Letters/Program.cs
@@ -0,0 +1,31 @@
+﻿//Program that takes a string and returns an ordered array containing the indices of all capital letters in the string
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<char> myOrderedArray = new List<char>();
+        string? sentence;
+
+        Console.Write("Enter a sentence: ");
+        sentence = Console.ReadLine();
+        char[] chars = sentence.ToCharArray();
+
+        foreach(char ch in chars)
+        {
+            if (char.IsUpper(ch))
+            {
+                myOrderedArray.Add(ch);
+                Console.WriteLine($"{ch} was found at index {sentence.IndexOf(ch)}");
+                
+            }
+        }
+
+        myOrderedArray.Sort();
+        foreach (char c in myOrderedArray)
+        {
+            Console.Write(c);
+        }
+    }
+
+}
