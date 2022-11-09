29d41c43640b96af62880f12336760528d002864
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-11-09 14:50:38 +0100

    Add files via upload

---
+++ A_Week_After/A_Week_After/Program.cs
@@ -0,0 +1,21 @@
+﻿//Function that takes in dates as strings and returns the date a week after
+
+class MyFunction
+{
+
+    public static void myFunction()
+    {
+        
+        Console.Write("Enter a date (eg 9/11/2022) and we'll tell you the date after a week: ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+
+        DateTime weekAfter = date.AddDays(7);
+        
+
+        Console.WriteLine($"Seven days from {date.ToString()} is {weekAfter.ToString()} ");
+    }
+    public static void Main(string[] args)
+    {
+       myFunction();
+    }
+}
