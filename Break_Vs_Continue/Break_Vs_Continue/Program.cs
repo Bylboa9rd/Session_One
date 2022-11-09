996f9e3f495fca6e66fcb1b48d134f67eb472864
Bylboa9rd <110336767+Bylboa9rd@users.noreply.github.com>
2022-11-09 14:49:45 +0100

    Add files via upload

---
+++ Break_Vs_Continue/Break_Vs_Continue/Program.cs
@@ -0,0 +1,28 @@
+﻿// Shows difference between break and continue, prints even numbers between 1 and 10
+
+internal class BreakVSContinue
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is an even number");
+                continue;
+            }
+
+            if (i == 9)
+            {
+                break;
+            }
+
+        };
+
+
+
+
+    }
+}
+
